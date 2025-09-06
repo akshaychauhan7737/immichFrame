5bbda2168315d5e3d3aa4fe169158c4fb29f0c5a
Akshay Chauhan <king.chauhan25@gmail.com>
2025-09-06 14:51:48 +0000

    .text-5xl {     font-size: 2.8rem; }

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
         '2xl': '1.75rem',
         '3xl': '2.2rem',
         '4xl': '2.6rem',
-        '5xl': '3rem',
+        '5xl': '2.8rem',
         '6xl': '4.4rem',
         '7xl': '5.5rem',
       },
